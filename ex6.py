2b504bda7017396b0d8d8f0bb76bafb4f2cf4327
paraujozr <pedroaraleegoo@gmail.com>
2025-03-24 20:17:58 -0300

    segundo commit

--- ex6.py
+++ ex6.py
@@ -1,7 +1,41 @@
-dias =  int(input("Valor em dias:"))
-horas =  int(input("Valor em horas:"))
-minutos =  int(input("Valor em minutos:"))
-segundos =   int(input("Valor em segundos:"))
+dias = int(input("Valor em dias:"))
+horas = int(input("Valor em horas:"))
+minutos = int(input("Valor em minutos:"))
+segundos = int(input("Valor em segundos:"))
+
+l1 = (dias * 86400)
+l2 = (horas * 3600)
+l3 = (minutos * 60)
+l4 = segundos
+
+soma = l1 + l2 + l3 + l4
+
+print("o valor em segundos é:", soma)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
