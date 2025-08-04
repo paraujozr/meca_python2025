07656535ec0e453c70507f522af871cdafd69a98
Pedro Araújo <pedroaraleegoo@gmail.com>
2025-08-04 20:48:09 -0300

    aula sobre listas

---
+++ aula 6/recapt.py
@@ -0,0 +1,48 @@
+nomes_alunos = []
+print(nomes_alunos)
+print(len(nomes_alunos))
+nomes_alunos = ["Leonardo T.", "Leonardo K.", "Giovanna", "Alanna" ]
+print(nomes_alunos)
+print(len(nomes_alunos))
+nomes_alunos[0] = "Leonardo Martins Trevisan"
+print(nomes_alunos)
+nomes_alunos[1] = "Leonardo Eiji Fabian Kowata"
+print(nomes_alunos)
+
+nomes_alunos_back = nomes_alunos
+print(nomes_alunos_back)
+
+nomes_alunos[2] = "Giovanna Carmona Roman Lopes"
+print(nomes_alunos)
+
+nomes_alunos_back = nomes_alunos[:]
+print(nomes_alunos_back)
+
+nomes_alunos[3] = "Allana Helena Campos Albino"
+print(nomes_alunos)
+
+print(nomes_alunos_back)
+print(nomes_alunos_back[:-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
